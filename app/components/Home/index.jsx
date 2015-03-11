2ad99eff74eed0eea4f80eff542a09903b7b6214
lern <mike@500px.com>
2015-03-11 11:28:06 -0400

    Remove heroSection

--- app/components/Home/index.jsx
+++ app/components/Home/index.jsx
@@ -6,14 +6,12 @@ var React        = require('react'),
 module.exports = React.createClass({
   render: function() {
     return <div className='homePage pageContent'>
-      <section className='heroSection'>
-        <h1>Homepage</h1>
-        <p>This is a sample project that uses React, Webpack and Material UI. It supports lightning-fast development ala live reload.</p>
-        <RaisedButton label='Try Me!' primary={true} onTouchTap={this._handleClick} />
-        <Snackbar
-          ref='snackbar'
-          message={'You did it!'} />
-      </section>
+      <h1>Homepage</h1>
+      <p>This is a sample project that uses React, Webpack and Material UI. It supports lightning-fast development ala live reload.</p>
+      <RaisedButton label='Try Me!' primary={true} onTouchTap={this._handleClick} />
+      <Snackbar
+        ref='snackbar'
+        message={'You did it!'} />
     </div>;
   },
 
